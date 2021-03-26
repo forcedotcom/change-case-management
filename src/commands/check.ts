624a15b45667cb06f8b731ccb3ec4e0cb888ef48
mshanemc <shane.mclaughlin@salesforce.com>
2021-03-25 19:38:10 -0500

    fix: check command always looks at case status

--- src/commands/check.ts
+++ src/commands/check.ts
@@ -6,7 +6,6 @@
  */
 
 import { Messages, SfdxError } from '@salesforce/core';
-import { env } from '@salesforce/kit';
 import { AnyJson } from '@salesforce/ts-types';
 import { ChangeCommand } from '../changeCommand';
 
@@ -16,10 +15,6 @@ Messages.importMessagesDirectory(__dirname);
 // Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
 // or any library that is using the messages framework can also be loaded this way.
 const messages = Messages.loadMessages('@salesforce/change-case-management', 'changecase');
-
-// ID for Standard Pre Approved
-const CHANGE_TYPE_ID = env.getString(ChangeCommand.getEnvVarFullName('CHANGE_TYPE_ID'), 'a8hB00000004DIzIAM');
-
 export default class Check extends ChangeCommand {
   public static description = messages.getMessage('check.description');
 
@@ -44,14 +39,10 @@ export default class Check extends ChangeCommand {
     const type = changeCase.SM_ChangeType__c;
     const status = changeCase.Status;
 
-    if (type === CHANGE_TYPE_ID) {
-      this.ux.log(`Release ${id} is standard pre-approved.`);
-    } else {
-      if (status !== 'Approved, Scheduled') {
-        throw new SfdxError(`The release ${id} is set to "${status}" and not approved.`);
-      }
-      this.ux.log(`Release ${id} is approved.`);
+    if (status !== 'Approved, Scheduled') {
+      throw new SfdxError(`The release ${id} is set to "${status}" and not approved.`);
     }
+    this.ux.log(`Release ${id} is approved.`);
     return { id, status, type };
   }
 
